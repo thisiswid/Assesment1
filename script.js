89869da100eed9490ec372a4c733361b7ef9d0ba
Widi <widiyamail5@gmail.com>
2024-04-03 13:56:40 +0700

    membuat array

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let buku = [
+    {"nama": "sherlock", "jumlah": 2},
+    {"nama": "lupin", "jumlah": 2},
+    {"nama": "aladin", "jumlah": 2},
+];
+
 function checkStock() {
     let inputJumlah = parseInt(document.getElementById('jumlah').value);
     let selectedBuku = document.getElementById('buku').value;
